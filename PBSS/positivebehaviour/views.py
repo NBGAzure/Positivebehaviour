5a5809df44484557d6b45c68049c60ae2b390828
Vipuldeep <vipuldeep95@gmail.com>
2019-09-08 03:04:22 +1000

    Template Implementation for index
    Minor errors regarding paths pops and gives warning but still it works completely fine.
    Requires major changes in layout and content.

--- PBSS/positivebehaviour/views.py
+++ PBSS/positivebehaviour/views.py
@@ -1,6 +1,8 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Theme coming soon</h1>")
+
+    return render(request, 'positivebehaviour/index.html')
