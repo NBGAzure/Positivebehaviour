cc4683ef91badd3a0efa971ba3fa54c68807df61
shankar6892 <shankarravindran@my.jcu.edu.au>
2019-09-09 22:40:46 +1000

    UserAuthentication
    UserAuthentication

--- PBSS/PBS/urls.py
+++ PBSS/PBS/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
--- PBSS/users/views.py
+++ PBSS/users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('positivebehaviour/index.html')
+            return redirect('index')
     else:
         form = UserCreationForm()
     return render(request,'users/register.html', {'form': form})
