b0545611f3e3b7a4963ca63d2287730b2748325b
shankar6892 <shankarravindran@my.jcu.edu.au>
2019-09-10 16:42:20 +1000

    UserAuthUpdate
    Updated UserAuth and also installed crispy forms

--- PBSS/PBS/settings.py
+++ PBSS/PBS/settings.py
@@ -38,6 +38,7 @@ STATICFILES_DIRS = [
 INSTALLED_APPS = [
     'positivebehaviour',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
