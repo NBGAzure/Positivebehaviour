a64849a46e1932b9835fca17d4b50f00eac49143
Vipuldeep <vipuldeep95@gmail.com>
2019-09-25 10:13:03 +1000

    FBA Form
    Anticident exception hanfdling error. Not resolved
    
    Co-Authored-By: VickyDesai <anninomi@users.noreply.github.com>

--- PBSS/fbaform/models.py
+++ PBSS/fbaform/models.py
@@ -7,6 +7,13 @@ class List(models.Model):
     def __str__(self):
         return self.item + ' | ' + str(self.completed)
 
+class Anticident(models.Model):
+    anticident = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.anticident + ' | ' + str(self.completed)
+
 #class Location(models.Model):
  #   wakingup = models.CharField(max_length=255)
     # pain = models.CharField(max_length=255)
