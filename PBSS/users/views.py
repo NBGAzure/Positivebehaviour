d712cdc587ba68fb2b7ee373e004723367a1ffba
shankar6892 <shankarravindran@my.jcu.edu.au>
2019-09-30 23:03:50 +1000

    Profiles_Picture

--- PBSS/users/views.py
+++ PBSS/users/views.py
@@ -12,9 +12,6 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account is now created for {username}!')
-
-            '''if crashed change userbase to index.  for redirecting after login.'''
-
             return redirect('index')
     else:
         form = UserRegisterForm()
@@ -25,13 +22,13 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
             messages.success(request, f'Account is now updated!')
             return redirect('profile')
-
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
