d23df928a9bc134f9f4c41887adbce8038e2811f
shankar6892 <shankarravindran@my.jcu.edu.au>
2019-10-01 19:24:13 +1000

    Hosting_heroku_Changes

--- PBSS/PBS/settings.py
+++ PBSS/PBS/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gc_et)$3w#shhdn6)y6%(pw=d00p6kief#_q-@o%(mdyuo7t#r'
-
+# SECRET_KEY = 'gc_et)$3w#shhdn6)y6%(pw=d00p6kief#_q-@o%(mdyuo7t#r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pbss.herokuapp.com']
 
 
 STATICFILES_DIRS = []
--- PBSS/env_vars.py
+++ PBSS/env_vars.py
@@ -3,6 +3,10 @@ import os
 aws_access_key = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 aws_storage_bucket_name = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+SECRET_KEY = "2eef9ae2be10278a8b822199c834f15cff06915d5e56ec87"
+DEBUG_VALUE = "True"
+
+
 print(aws_access_key)
 print(aws_secret_access_key)
 print(aws_storage_bucket_name)
