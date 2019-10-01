97645b93414327174f292b2cb121aecda455d33f
shankar6892 <shankarravindran@my.jcu.edu.au>
2019-10-02 09:35:32 +1000

    Profile_edit

--- PBSS/users/views.py
+++ PBSS/users/views.py
@@ -41,5 +41,26 @@ def profile(request):
     return render(request, 'users/profile.html', context, {'title': 'Profile'})
 
 
+
+
+
 def edit_profile(request):
-    return render(request, "users/edit_profile.html")
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account is now updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, "users/edit_profile.html", context, {'title': 'Profile'})
