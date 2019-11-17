2cb7bf55546d0557c5f5b1348cf723092c0c7e40
shankar6892 <shankarravindran@my.jcu.edu.au>
2019-11-18 08:22:14 +1000

    AWS_StsticChanges
    Env Variable were commented and static codes were created

--- PBSS/PBS/settings.py
+++ PBSS/PBS/settings.py
@@ -147,9 +147,21 @@ LOGIN_URL = 'login'
 "EMAIL_HOST_USER = os.environ.get('EMAIL_USER') %}"
 "EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')"
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+
+
+#AWS Static Connection
+AWS_ACCESS_KEY_ID = 'AKIASZS3DSG2EJ52NT47'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'positive-behaviour-support'
+
+
+
+#AWS connection using env variables
+
+#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+#AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
