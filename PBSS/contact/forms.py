baf8d11d07963c2cbc26ab1a11b33bd273586d67
Vipuldeep <vipuldeep95@gmail.com>
2019-11-24 21:39:33 +1000

    Revert "Merge branch 'beta' of https://github.com/anninomi/Positivebehaviour into beta"
    This reverts commit 6097f6b446b7853708e62b6b679bd1d85f9b7377, reversing
    changes made to 15c6c30d9c0529068696271230d015f8b767dd6f.

--- PBSS/contact/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from .models import Contact
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = {'first_name', 'last_name', 'email', 'message'}
