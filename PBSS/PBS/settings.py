37f6e94f78f5336fa45b4c7136cba0dbee8cc3a8
Vipuldeep <vipuldeep95@gmail.com>
2019-12-02 11:27:37 +1000

    revert back SQLlite

--- PBSS/PBS/settings.py
+++ PBSS/PBS/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'PBS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'newpbssdatabase',
-        'USER': 'root',
-        'PASSWORD': 'myPassword123',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'newpbssdatabase',
+#         'USER': 'root',
+#         'PASSWORD': 'myPassword123',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
