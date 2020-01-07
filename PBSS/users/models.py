442247eade042055baedf368136b2eef32ed9bb9
Vipuldeep <vipuldeep95@gmail.com>
2020-01-07 13:00:13 +1000

    New feilds added to client

--- PBSS/users/models.py
+++ PBSS/users/models.py
@@ -14,10 +14,16 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-
+    GENDER_CHOICES = (
+        (u'M', u'Male'),
+        (u'F', u'Female'),
+    )
     client_name = models.CharField(max_length=100)
-
+    DOB = models.DateField(auto_now_add=False, auto_now=False, blank=True)
     email = models.EmailField()
+    location = models.CharField(max_length=100, null=True)
+    history = models.CharField(max_length=100, null=True)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, null=True)
     content = models.TextField()
     date_issued = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
