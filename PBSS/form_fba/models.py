7ef692efff79882749343d34a72d9c6d6c8d2589
shankar6892 <shankarravindran@my.jcu.edu.au>
2020-01-17 11:26:37 +1000

    Max Length of Intensity
    Still needs to be worked on...

--- PBSS/form_fba/models.py
+++ PBSS/form_fba/models.py
@@ -2,20 +2,23 @@ from django.forms import ModelForm, Textarea
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+
 class Fba(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="fba", null=True)
-    anticedent = models.CharField(max_length=200)
-    behaviour = models.CharField(max_length=200)
-    consequence = models.CharField(max_length=200)
-    intensity = models.IntegerField()
+    anticedent = models.CharField(blank=False, max_length=200)
+    behaviour = models.CharField(blank=False, max_length=200)
+    consequence = models.CharField(blank=False, max_length=200)
+    intensity = models.PositiveSmallIntegerField(blank=False, validators=[MinValueValidator(0), MaxValueValidator(10)])
 
     def get_absolute_url(self):
         return reverse("forms:detail", kwargs={"is": self.id})
 
 
-
 class Meta:
     db_table = 'fba'
+
+
