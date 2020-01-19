d5fab67fc7e5a1652700de3e4fe742da7987a6ae
Vipuldeep <vipuldeep95@gmail.com>
2020-01-19 14:50:45 +1000

    contact us prompt missing, corrected

--- PBSS/contact/views.py
+++ PBSS/contact/views.py
@@ -5,7 +5,9 @@ from .forms import ContactForm
 def contact(request):
         form = ContactForm(request.POST or None)
         if form.is_valid():
+            messages.success(request, 'Your request for contact has been updated')
             form.save()
+
         context ={
             'form': form
         }
